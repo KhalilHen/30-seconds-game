e62904846939834de8abd79398c9fc7d8574c64e
Khalil <khalilhenane@hotmail.com>
2024-08-18 23:39:18 +0200

    Create some scenes and buttons. Worked on adding a panel based on button clicks

---
+++ Assets/Scripts/exit.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class exit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkIfButtonIsPressed();
+
+
+
+    }
+
+ void    checkIfButtonIsPressed()
+    {
+
+        foreach (Touch touch in Input.touches)
+        {
+
+            if (touch.fingerId == 0)
+
+            {
+                SceneManager.LoadSceneAsync(0);
+
+            }
+        }
+}
